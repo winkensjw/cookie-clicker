42fa771e5d2e41e513843de46ebec83672ade3d7
winkensjw <84124931+winkensjw@users.noreply.github.com>
2025-02-28 13:48:40 +0100

    Add main menu

--- Entities/MainMenu/main_menu.gd
+++ Entities/MainMenu/main_menu.gd
@@ -1,1 +1,8 @@
 extends CanvasLayer
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_pressed():
+		close_main_menu()
+		
+func close_main_menu() -> void:
+		Events.main_menu_closed.emit()
